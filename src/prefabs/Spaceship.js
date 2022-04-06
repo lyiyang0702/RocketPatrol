61dc51118f652ac151fafec2ab981342f832576d
Yiyang Lu <lyiyang@ucsc.edu>
2022-04-05 18:20:58 -0700

    Finished

---
+++ src/prefabs/Spaceship.js
@@ -0,0 +1,25 @@
+// Spaceship prefab
+class Spaceship extends Phaser.GameObjects.Sprite {
+    constructor (scene,x,y,texture,frame, pointValue){
+        super (scene,x,y,texture,frame);
+
+        // add object to existing scene
+        scene.add.existing (this);
+        this.points = pointValue; // store pointValue
+        this.moveSpeed = game.settings.spaceshipSpeed;
+    }
+
+    update (){
+        // move spaceship left
+        this.x -= this.moveSpeed;
+        // wrap around from left edge to right edge
+        if (this.x <= 0 - this.width) {
+            this.reset();
+        }
+    }
+
+    // position reset
+    reset () {
+        this.x = game.config.width;
+    }
+}
